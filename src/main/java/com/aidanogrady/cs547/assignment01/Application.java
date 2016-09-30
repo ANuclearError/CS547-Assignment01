f9360ceeceb7fb13522b81f979545e54650fd635
Aidan O'Grady <aidan@aidanogrady.com>
2016-09-30 16:14:34 +0100

    Added validation to properties.

--- src/main/java/com/aidanogrady/cs547/assignment01/Application.java
+++ src/main/java/com/aidanogrady/cs547/assignment01/Application.java
@@ -24,18 +24,63 @@ public class Application {
             Properties properties = new Properties();
             try {
                 properties.load(new FileInputStream(args[0]));
-
-                Search[] searches = new Search[2];
-                searches[0] = new HillClimbingSearch();
-                searches[1] = new GeneticAlgorithmSearch();
-                for (Search search : searches) {
-                    search.benchmark(properties);
-                    System.out.println();
+                if (validateProperties(properties)) {
+                    Search[] searches = new Search[2];
+                    searches[0] = new HillClimbingSearch();
+                    searches[1] = new GeneticAlgorithmSearch();
+                    for (Search search : searches) {
+                        search.benchmark(properties);
+                        System.out.println();
+                    }
                 }
 
             } catch (IOException e) {
                 e.printStackTrace();
             }
         }
+    }
+
+    /**
+     * Validates the config properties are valid.
+     *
+     * @param properties the properties to validate.
+     * @return valid
+     */
+    private static boolean validateProperties(Properties properties) {
+        String[] props = {"target", "benchmark", "hill.steps", "hill.size",
+                "ga.population", "ga.elitism", "ga.mutation", "ga.tournament"
+        };
+
+        String[] types = {"string", "int", "int", "int", "int", "double",
+                "double", "int"
+        };
+
+        for (int i = 0; i < props.length; i++) {
+            String prop = props[i];
+            String value = properties.getProperty(prop);
+            if (value == null) {
+                System.out.println(prop + " property does not exist in file.");
+            }
+            switch (types[i]) {
+                case "int":
+                    try {
+                        int val = Integer.parseInt(value);
+                    } catch (NumberFormatException e) {
+                        System.out.println(prop + " must be " + types[i]);
+                        return false;
+                    }
+                    break;
+                case "double":
+                    try {
+                        double val = Double.parseDouble(value);
+                    } catch (NumberFormatException e) {
+                        System.out.println(prop + " must be " + types[i]);
+                        return false;
+                    }
+                    break;
+
+            }
+        }
+        return true;
     }
 }
